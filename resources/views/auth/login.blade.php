983d2f0228d768af246db89ed350cb825ab8ea5e
Yogaaprtma <111202113472@mhs.dinus.ac.id>
2025-09-12 09:44:49 +0700

    modif login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,17 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="{{ asset('assets/logo-medsos.png') }}">
+    <style>
+        .eye-icon {
+            transition: opacity 0.3s ease-in-out;
+        }
+        .hidden-eye {
+            opacity: 0;
+        }
+        .visible-eye {
+            opacity: 1;
+        }
+    </style>
 </head>
 <body style="background-color: black; color: white;">
 
@@ -37,8 +48,13 @@
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
+
                     <div class="mb-3">
                         <label for="password" class="form-label fw-bold">Password</label>
+                        <span class="ms-2" id="togglePassword">
+                            <i class="far fa-eye eye-icon text-light" id="eyeIcon" style="cursor: pointer;"></i>
+                            <i class="fas fa-eye-slash eye-icon text-light hidden-eye" id="eyeSlashIcon" style="cursor: pointer;"></i>
+                        </span>
                         <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Masukkan password" required>
                         @error('password')
                             <div class="text-danger">{{ $message }}</div>
@@ -55,6 +71,37 @@
         </div>
     </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
+<script>
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+    const eyeIcon = document.querySelector('#eyeIcon');
+    const eyeIconSlash = document.querySelector('#eyeIconSlash'); // Icon mata slash
+
+    togglePassword.addEventListener('click', function () {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+
+        // Toggle visibility icon mata
+        eyeIcon.classList.toggle('hidden-eye');
+        eyeIconSlash.classList.toggle('hidden-eye');
+    });
+
+    const toggleConfirmPassword = document.querySelector('#toggleConfirmPassword');
+    const konfirmasiPassword = document.querySelector('#password_confirmation');
+    const confirmEyeIcon = document.querySelector('#confirmEyeIcon');
+    const confirmEyeSlashIcon = document.querySelector('#confirmEyeSlashIcon');
+
+    toggleConfirmPassword.addEventListener('click', function () {
+        const type = konfirmasiPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        konfirmasiPassword.setAttribute('type', type);
+
+        // Toggle visibility icon mata
+        confirmEyeIcon.classList.toggle('hidden-eye');
+        confirmEyeSlashIcon.classList.toggle('hidden-eye');
+    });
+</script>
 
 <footer class="footer sticky-bottom mt-auto pt-5" style="background-color: white;">
     <div class="container">
